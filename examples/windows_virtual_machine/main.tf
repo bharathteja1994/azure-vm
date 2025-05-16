5e70782100e5aa44209293dee8f9390edd72495c
bharathteja1994 <77113760+bharathteja1994@users.noreply.github.com>
2025-05-16 22:59:46 +0530

    Add files via upload

--- examples/windows_virtual_machine/main.tf
+++ examples/windows_virtual_machine/main.tf
@@ -1,1 +1,59 @@
-test
+# Azurerm provider configuration
+provider "azurerm" {
+  features {}
+}
+
+data "azurerm_log_analytics_workspace" "example" {
+  name                = "loganalytics-we-sharedtest2"
+  resource_group_name = "rg-shared-westeurope-01"
+}
+
+module "virtual-machine" {
+  source  = "../.."
+  version = "2.3.0"
+
+  # Resource Group, location, VNet and Subnet details
+  resource_group_name  = "rg-shared-westeurope-01"
+  location             = "westeurope"
+  virtual_network_name = "vnet-shared-hub-westeurope-001"
+  subnet_name          = "snet-management"
+  virtual_machine_name = "win-machine"
+  os_flavor                 = "windows"
+  windows_distribution_name = "windows2019dc"
+  virtual_machine_size      = "Standard_A2_v2"
+  admin_username            = "azureadmin"
+  admin_password            = "P@$$w0rd1234!"
+  instances_count           = 1
+  enable_proximity_placement_group = true
+  enable_vm_availability_set       = true
+  nsg_inbound_rules = [
+    {
+      name                   = "rdp"
+      destination_port_range = "3389"
+      source_address_prefix  = "*"
+    },
+    {
+      name                   = "http"
+      destination_port_range = "80"
+      source_address_prefix  = "*"
+    },
+  ]
+  enable_boot_diagnostics = true
+  data_disks = [
+    {
+      name                 = "disk1"
+      disk_size_gb         = 100
+      storage_account_type = "StandardSSD_LRS"
+    },
+    {
+      name                 = "disk2"
+      disk_size_gb         = 200
+      storage_account_type = "Standard_LRS"
+    }
+  ] 
+  log_analytics_workspace_id = data.azurerm_log_analytics_workspace.example.id
+  deploy_log_analytics_agent                 = true
+  log_analytics_customer_id                  = data.azurerm_log_analytics_workspace.example.workspace_id
+  log_analytics_workspace_primary_shared_key = data.azurerm_log_analytics_workspace.example.primary_shared_key
+  tags = {}
+}
